8936245bb0af8127fd8ed48d179ebfc6f0aa09eb
Tarek <https://github.com/Tarek-Anwar/TourGuideApp>
2021-12-18 17:05:38 +0200

    first commit

---
+++ app/src/main/java/com/HomeGarage/garage/sign/sign.java
@@ -0,0 +1,17 @@
+package com.HomeGarage.garage.sign;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.HomeGarage.garage.R;
+
+public class sign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign);
+
+    }
+}
---
+++ app/src/main/java/com/HomeGarage/garage/sign/signFragment.java
@@ -0,0 +1,34 @@
+package com.HomeGarage.garage.sign;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.HomeGarage.garage.R;
+
+public class signFragment extends Fragment {
+
+
+    public signFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign, container, false);
+    }
+
+}
