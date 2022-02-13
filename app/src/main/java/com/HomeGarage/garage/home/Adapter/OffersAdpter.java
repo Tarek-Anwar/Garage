e97a79d5a93302fb8bae6d6417455ecce77e62a9
Tarek <https://github.com/Tarek-Anwar/TourGuideApp>
2022-02-13 14:54:44 +0200

    new offers recycleview

--- app/src/main/java/com/HomeGarage/garage/home/Adapter/OffersAdpter.java
+++ app/src/main/java/com/HomeGarage/garage/home/Adapter/OffersAdpter.java
@@ -1,40 +1,35 @@
 package com.HomeGarage.garage.home.Adapter;
 
 import android.content.Context;
-import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.HomeGarage.garage.R;
 import com.HomeGarage.garage.home.models.OffersModels;
-
 import java.util.ArrayList;
-import java.util.List;
 
-public class offersAdpter extends RecyclerView.Adapter<offersAdpter.offersViewHolder> {
+public class OffersAdpter extends RecyclerView.Adapter<OffersAdpter.OffersViewHolder> {
 
     ArrayList<OffersModels> modelsList;
     Context context ;
 
-    public offersAdpter(ArrayList<OffersModels> modelsList, Context context) {
+    public OffersAdpter(ArrayList<OffersModels> modelsList, Context context) {
         this.modelsList = modelsList;
         this.context = context;
     }
 
     @NonNull
     @Override
-    public offersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.offers_layout,parent,false);
-        return new offersViewHolder(view);
+    public OffersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.offers_row_layout,parent,false);
+        return new OffersViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull offersViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull OffersViewHolder holder, int position) {
         holder.BulidUi(modelsList.get(position));
     }
 
@@ -43,11 +38,10 @@ public class offersAdpter extends RecyclerView.Adapter<offersAdpter.offersViewHo
         return modelsList.size();
     }
 
-
-    protected static final class offersViewHolder extends RecyclerView.ViewHolder {
+    protected static final class OffersViewHolder extends RecyclerView.ViewHolder {
 
         private  ImageView imageView;
-        public offersViewHolder(@NonNull View itemView) {
+        public OffersViewHolder(@NonNull View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.offer_img);
         }
