8f1ecd0f346c2aea52eb35abd1b7941a0873165c
Tarek <https://github.com/Tarek-Anwar/TourGuideApp>
2022-02-13 14:54:44 +0200

    new offers recycleview

--- app/src/main/java/com/HomeGarage/garage/home/HomeActivity.java
+++ app/src/main/java/com/HomeGarage/garage/home/HomeActivity.java
@@ -16,6 +16,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         /*framehome = findViewById(R.id.framehome);
         homeFragment =new HomeFragment();
         getSupportFragmentManager().
--- app/src/main/java/com/HomeGarage/garage/home/HomeFragment.java
+++ app/src/main/java/com/HomeGarage/garage/home/HomeFragment.java
@@ -1,41 +1,39 @@
 package com.HomeGarage.garage.home;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.HomeGarage.garage.R;
 import com.HomeGarage.garage.home.models.OffersModels;
 import com.HomeGarage.garage.home.Adapter.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class HomeFragment extends Fragment {
+
     ArrayList <OffersModels> offersModels = new ArrayList<>();
-    RecyclerView recyclerView;
-    public HomeFragment() {
-    }
+    RecyclerView recyclerOffers;
 
-
+    public HomeFragment() { }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View root =  inflater.inflate(R.layout.fragment_home, container, false);
-        recyclerView = root.findViewById(R.id.recycle_offers);
 
+        //find element recyclerOffers
+        recyclerOffers = root.findViewById(R.id.recycle_offers);
+
+        // add item to recyclerOffers
         offersModels.add(new OffersModels(R.drawable.ic_launcher_background));
         offersModels.add(new OffersModels(R.drawable.ic_launcher_foreground));
         offersModels.add(new OffersModels(R.drawable.ic_launcher_background));
@@ -46,11 +44,11 @@ public class HomeFragment extends Fragment {
         offersModels.add(new OffersModels(R.drawable.ic_launcher_foreground));
 
 
-        offersAdpter adapter = new offersAdpter(offersModels,getContext());
         //put LinearLayoutManager to recyclerView
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext() , RecyclerView.HORIZONTAL , false));
+        recyclerOffers.setLayoutManager(new LinearLayoutManager(getContext() , RecyclerView.HORIZONTAL , false));
         //set adapter
-        recyclerView.setAdapter(adapter);
+        recyclerOffers.setAdapter(new OffersAdpter(offersModels,getContext()));
+
         return root;
 
     }
