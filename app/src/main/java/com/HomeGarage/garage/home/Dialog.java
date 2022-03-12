d25ec83addb1164509ca08879834df59e22d1d76
Tarek <https://github.com/Tarek-Anwar/TourGuideApp>
2022-03-12 03:40:43 +0200

    add balance to all

--- app/src/main/java/com/HomeGarage/garage/home/Dialog.java
+++ app/src/main/java/com/HomeGarage/garage/home/Dialog.java
@@ -1,7 +1,7 @@
 package com.HomeGarage.garage.home;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,8 +18,6 @@ import com.HomeGarage.garage.R;
 import com.HomeGarage.garage.home.models.CarInfo;
 import com.HomeGarage.garage.home.models.GrageInfo;
 import com.HomeGarage.garage.home.models.Opreation;
-import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -61,11 +59,11 @@ public class Dialog extends DialogFragment {
         View root = inflater.inflate(R.layout.pay_dialog, container, false);
         initViews(root);
 
-
         String txt = balance.getText().toString();
         String costTxt = cost.getText().toString();
 
         getCarInfo(new OnBalanceReciveCallback() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void OnBalanceRecive(CarInfo carInfo) {
                 calc.setOnClickListener(v -> {
@@ -81,46 +79,44 @@ public class Dialog extends DialogFragment {
                 });
             }
 
+            @SuppressLint("SetTextI18n")
             @Override
             public void OnBalanceAppRecive(Float balanceApp) {
-                pay.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        //update car owner balance
-                       carInfo.setBalance(carInfo.getBalance() - costIN);
+                pay.setOnClickListener(v -> {
+                    //update car owner balance
+                    carInfo.setBalance(carInfo.getBalance() - costIN);
 
-                        reference.child("balance").setValue(carInfo.getBalance());
-                        balance.setText(txt + "" + carInfo.getBalance());
+                    reference.child("balance").setValue(carInfo.getBalance());
+                    balance.setText(txt + "" + carInfo.getBalance());
 
-                        //calc app and grage balance
-                        float appBalance = (float) (costIN * .1);
-                        float grageBalance = costIN - appBalance;
+                    //calc app and grage balance
+                    float appBalance = (float) (costIN * .1);
+                    float grageBalance = costIN - appBalance;
 
-                        //update balance for grage owner
-                        grageInfo.setBalance(grageInfo.getBalance() + grageBalance);
-                        garageReference.child(grageInfo.getId()).child("balance").setValue(grageInfo.getBalance());
+                    //update balance for grage owner
+                    grageInfo.setBalance(grageInfo.getBalance() + grageBalance);
+                    garageReference.child(grageInfo.getId()).child("balance").setValue(grageInfo.getBalance());
 
-                        //update app balance
-                        appReference.child("Balance").setValue(appBalance+balanceApp);
+                    //update app balance
+                    appReference.child("Balance").setValue(appBalance+balanceApp);
 
-                        //creat opreation and save to last opreation list
-                        Opreation opreation = new Opreation();
-                        Date date = new Date(System.currentTimeMillis());
-                        String dateOpreation = formatterLong.format(date);
-                        opreation.setDate(dateOpreation);
-                        opreation.setState("3");
-                        opreation.setType("4");
-                        opreation.setFrom(FirebaseUtil.firebaseAuth.getUid());
-                        opreation.setTo(grageInfo.getId());
-                        opreation.setPrice(costIN);
-                        opreation.setFromName(FirebaseUtil.carInfoLogin.get(0).getName());
-                        opreation.setToName(grageInfo.getNameEn());
-                        opreation.setId(referenceOperattion.push().getKey());
-                        referenceOperattion.child(opreation.getId()).setValue(opreation);
+                    //creat opreation and save to last opreation list
+                    Opreation opreation = new Opreation();
+                    Date date = new Date(System.currentTimeMillis());
+                    String dateOpreation = formatterLong.format(date);
+                    opreation.setDate(dateOpreation);
+                    opreation.setState("3");
+                    opreation.setType("4");
+                    opreation.setFrom(FirebaseUtil.firebaseAuth.getUid());
+                    opreation.setTo(grageInfo.getId());
+                    opreation.setPrice(costIN);
+                    opreation.setFromName(FirebaseUtil.carInfoLogin.get(0).getName());
+                    opreation.setToName(grageInfo.getNameEn());
+                    opreation.setId(referenceOperattion.push().getKey());
+                    referenceOperattion.child(opreation.getId()).setValue(opreation);
 
-                        opreations.add(opreation);
-                        Toast.makeText(getContext(), "thanks", Toast.LENGTH_SHORT).show();
-                    }
+                    opreations.add(opreation);
+                    Toast.makeText(getContext(), "thanks", Toast.LENGTH_SHORT).show();
                 });
 
             }
--- app/src/main/java/com/HomeGarage/garage/home/reservation/RequstActiveFragment.java
+++ app/src/main/java/com/HomeGarage/garage/home/reservation/RequstActiveFragment.java
@@ -1,25 +1,38 @@
 package com.HomeGarage.garage.home.reservation;
 
+import android.os.Build;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Chronometer;
 import android.widget.ProgressBar;
 
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+
+import com.HomeGarage.garage.FirebaseUtil;
 import com.HomeGarage.garage.R;
+import com.HomeGarage.garage.databinding.FragmentRequstActiveBinding;
 import com.HomeGarage.garage.home.models.Opreation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 
 public class RequstActiveFragment extends Fragment {
 
+    FragmentRequstActiveBinding binding;
     Opreation opreation;
-    Chronometer chronometer;
-    ProgressBar progressBar;
+    volatile boolean con;
+
     int countProgress ;
+    SimpleDateFormat formatterLong =new SimpleDateFormat("dd/MM/yyyy hh:mm aa" , new Locale("en"));
 
     public RequstActiveFragment(Opreation opreation) {
         this.opreation = opreation;
@@ -28,43 +41,51 @@ public class RequstActiveFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
 
-        /*Date start = null;
-            try {
-                start = formatterLong.parse(opreation.getDate());
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            Long diff = System.currentTimeMillis() - start.getTime();
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-            countProgress = (int) (diff / 10000);
-            chronometer.setBase(SystemClock.elapsedRealtime() - diff);
-            chronometer.start();
-        countProgress = 1;
-        chronometer.setBase(SystemClock.elapsedRealtime());
-        chronometer.start();
+        binding = FragmentRequstActiveBinding.inflate(getLayoutInflater());
+        binding.txtDate.setText(opreation.getDate());
+        binding.txtGarageName.setText(opreation.getToName());
+        binding.txtRequstStateHome.setText(FirebaseUtil.stateList.get(Integer.parseInt(opreation.getState())-1));
+        binding.txtRequstTypeHome.setText(FirebaseUtil.typeList.get(Integer.parseInt(opreation.getType())-1));
 
-        progressBar.setMin(0);
-        progressBar.setMax(200);
+        Date start = null;
+            try { start = formatterLong.parse(opreation.getDate());
+            } catch (ParseException e) { e.printStackTrace(); }
+
+        Long diff = System.currentTimeMillis() - start.getTime();
+
+        countProgress = (int) (diff / 10000);
+        con = true;
+        binding.chronometer.setBase(SystemClock.elapsedRealtime() - diff);
+        binding.chronometer.start();
+        binding.progressBar.setMin(0);
+        binding.progressBar.setMax(1100);
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                if(countProgress<=200){
-                    progressBar.setProgress(countProgress);
+           if(countProgress<1100){
+                    binding.progressBar.setProgress(countProgress);
                     countProgress++;
                     handler.postDelayed(this,1000);}
-                else {
+                else if(countProgress==1100){
+                    if(con){ binding.progressBar.setProgressDrawable(getResources().getDrawable(R.drawable.cricle_tow));con = false; }
+                    else { binding.progressBar.setProgressDrawable(getResources().getDrawable(R.drawable.cricle_progress_bar));con = true; }
+                    countProgress=0;
+                    handler.postDelayed(this,1000);
+                }
+                else{
                     handler.removeCallbacks(this);
                 }
             }
-        },1000);*/
-    }
+        },10000);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_requst_active, container, false);
+        return binding.getRoot();
     }
 }
